c72fc11e1db0ccd7e6144304d83072f66088284f
danybl <dany.benoit.lafond@gmail.com>
2015-04-13 10:05:38 -0400

    le retour du roi

---
+++ EcoleWeb/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EcoleWeb.Models
+{
+
+    public class AppDbContext : IdentityDbContext<etudiant>
+    {
+        public AppDbContext()
+            : base("ecoleEntities")
+        {
+        }
+
+    }
+}
