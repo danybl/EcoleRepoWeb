9942e404faef38b0824716622d9b70dc2c2bc863
danybl <dany.benoit.lafond@gmail.com>
2015-04-13 09:41:33 -0400

    retrait accountmodels

--- EcoleWeb/Models/StudentsViewModel.cs
+++ EcoleWeb/Models/StudentsViewModel.cs
@@ -80,32 +80,4 @@ namespace EcoleWeb.Models
         public string ConfirmPassword { get; set; }
     }
 
-    public class ResetPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Courrier électronique")]
-        public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Mot de passe")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirmer le mot de passe")]
-        [Compare("Password", ErrorMessage = "Le nouveau mot de passe et le mot de passe de confirmation ne correspondent pas.")]
-        public string ConfirmPassword { get; set; }
-
-        public string Code { get; set; }
-    }
-
-    public class ForgotPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "E-mail")]
-        public string Email { get; set; }
-    }
 }
