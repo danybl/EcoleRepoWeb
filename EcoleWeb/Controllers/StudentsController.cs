27485e04e3877440c578e0a50e71dfee8c2b65ac
danybl <dany.benoit.lafond@gmail.com>
2015-04-13 09:02:43 -0400

    retrait de identity

--- EcoleWeb/Controllers/StudentsController.cs
+++ EcoleWeb/Controllers/StudentsController.cs
@@ -7,10 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using EcoleWeb.Models;
+using System.Threading.Tasks;
 
 namespace EcoleWeb.Controllers
 {
-    [Authorize (Roles="admin")]
+    [Authorize(Roles = "admin")]
     public class StudentsController : Controller
     {
         private ecoleEntities db = new ecoleEntities();
@@ -143,6 +144,68 @@ namespace EcoleWeb.Controllers
             }
             return View(etudiant);
         }
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToLocal(returnUrl);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        //
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                return RedirectToAction("Index", "Home");
+
+
+            }
+
+            // Si nous sommes arrivés là, un échec s’est produit. Réafficher le formulaire
+            return View(model);
+        }
+
 
     }
 }
